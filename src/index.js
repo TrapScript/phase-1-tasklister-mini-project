bcbbec8053b77424a2bb295dbb043a2d5d28435e
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-07-09 20:54:14 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+   /event.preventDefault();
+
+    const task = input.value;
+
+    const listItem = document.createElement("li");
+    listItem.textContent = task;
+
+    taskList.appendChild(listItem);
+    input.value = "";
+
+ }
+ function addTAsk(){
+    if(inputBox.value === ''){
+      alert("write something");
+    }
+    else{
+      let li =document.createElement("li");
+      li.innerHTML = inputBox.value;
+      listContainer.appendChild(li);
+    }
+ }
+
 });
